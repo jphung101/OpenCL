f473bca681c716abd7d8beeca18887b793901953
jphung101 <jasonphung101@gmail.com>
2025-02-21 18:56:37 -0800

    Adding partial skeleton OpenCL setup code; updated OpenCL makefile

--- Makefile
+++ Makefile
@@ -1,8 +1,9 @@
 CC = g++
-CFLAGS = -O3
+CFLAGS = -O0
+LDFLAGS = -framework OpenCL # Learn how frameworks work in make for macOS
 
 macOS: macOS.cpp
-	$(CC) $(CFLAGS) -o macOS macOS.cpp
+	$(CC) $(CFLAGS) $(LDFLAGS) -o macOS macOS.cpp 
 
 clean:
 	rm macOS
--- macOS.cpp
+++ macOS.cpp
@@ -1,24 +1,59 @@
-//This file is an attempt to get OpenCL working on an ARM Mac (specifically M1)
+// This file is an attempt to get OpenCL working on an ARM Mac (specifically M1)
 #include <iostream>
+#include <string>
 #include <OpenCL/cl.h>
 #include <time.h> // for timing
 
 using namespace std;
 
-int main(){
-    clock_t start, end;
-    double time;
-    int size = 1000000;
-    int arr[1000000] = {0};
-    start = clock();
+void check(cl_int err);
 
-    
-    for(int i = 0; i < size; i++){
-        arr[i]++;
+
+int main(int argc, char *argv[]){
+    cl_int err; // used to catch errors
+    cl_platform_id platforms; // not needed, as we only have 1 platform (?). Not exactly sure what multiple platforms entail
+
+    int num = 1; // number of devices you expect your platform to have
+    cl_device_id device;
+    cl_device_id devices[num]; // holds CPU/GPU options
+    cl_uint deviceCount; // number of devices that are availble 
+
+    // DEVICE ID:
+    err = clGetDeviceIDs(nullptr, CL_DEVICE_TYPE_ALL, num, devices, &deviceCount);
+    printf("Device count: %d\n", deviceCount); // Considered `CL_DEVICE_TYPE_GPU`
+    check(err);
+
+    if(num == 1 || argc == 1){
+        device = devices[0];
+    }else{
+        device = devices[stoi(argv[1])];
     }
 
-    end = clock();
-    time = ((double) (end - start)) / CLOCKS_PER_SEC * 1000; // Convert to milliseconds
-    printf("%f", time);
+    // DEVICE INFO:
+    string name;
+    err = clGetDeviceInfo(device, CL_DEVICE_NAME, sizeof(name), &name, nullptr);
+    printf("Device name: %s\n", name.c_str());
+    check(err);
 
+    // CONTEXT:
+    cl_context context;
+    context = clCreateContext(nullptr, 1, &device, nullptr, nullptr, &err);
+    check(err);
+
+    // COMMAND QUEUE:
+    cl_command_queue queue;
+    queue = clCreateCommandQueue(context, device, 0, &err); // TODO: figure out what the "0" is
+    check(err);
+
+    
+
+
+
+    return 0;
+}
+
+void check(cl_int err){
+    if(err != CL_SUCCESS){
+        printf("Error: %d\n", err);
+    }
 }
